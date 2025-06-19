b05e7a3391c6f3b9bae995cc1f4340b2691e03fa
Kishan <kishansoni45285@gmail.com>
2025-06-19 11:01:06 +0530

    Added k8s URLs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,9 @@ import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 import React from 'react';
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL;
+
+
 
 function App() {
   return (
